d4f33041c598f0d65c73df8594f41dae44b30466
liudng <liudng@gmail.com>
2015-07-23 17:27:16 +0800

    Fix test case.

--- query_test.go
+++ query_test.go
@@ -5,35 +5,35 @@
 package db
 
 import (
-    "testing"
-    "io/ioutil"
-    "fmt"
-    "strings"
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"testing"
 )
 
 type QueryTest struct {
-    // Query
-    Query *Query
+	// Query
+	Query *Query
 }
 
 func (qt *QueryTest) Init(t *testing.T) {
-    // Load sql file
-    p := fmt.Sprintf("tb-%s.sql", qt.Query.Server.Type)
-    b, err := ioutil.ReadFile(p)
-    if err != nil {
-        t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
-    }
-    initSQL := strings.Split(string(b), ";")
+	// Load sql file
+	p := fmt.Sprintf("tb-%s.sql", qt.Query.Server.Type)
+	b, err := ioutil.ReadFile(p)
+	if err != nil {
+		t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
+	}
+	initSQL := strings.Split(string(b), ";")
 
-    // Run Init SQL
-    for _, v := range initSQL {
-        if strings.Trim(v, "\r\n \t") != "" {
-            _, err := qt.Query.Server.Exec(v)
-            if err != nil {
-                t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
-            }
-        }
-    }
+	// Run Init SQL
+	for _, v := range initSQL {
+		if strings.Trim(v, "\r\n \t") != "" {
+			_, err := qt.Query.Server.Exec(v)
+			if err != nil {
+				t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
+			}
+		}
+	}
 }
 
 func (qt *QueryTest) Load(t *testing.T) {
@@ -41,207 +41,201 @@ func (qt *QueryTest) Load(t *testing.T) {
 }
 
 func (qt *QueryTest) Start(t *testing.T) {
-    qt.Insert(t)
-    qt.Update(t)
-    qt.Rows(t)
-    qt.Delete(t)
+	qt.Insert(t)
+	qt.Update(t)
+	qt.Rows(t)
+	qt.Delete(t)
 }
 
 func (qt *QueryTest) Insert(t *testing.T) {
-    // Insert
-    q := NewQuery(qt.Query.Server)
-    q.InsertInto("passport_user")
-    q.SetPrimary("UserID") // PostgreSQL compatibility
-    r, err := q.Fields("UserID", "CreationTime", "BirthYear", "Gender", "Nickname").Values(1000000, "2015-01-17 00:00:00", 1980, "Male", "肯·汤普逊").Exec()
-    if err != nil {
-        t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
-    }
-    if r.LastInsertId != 1000000 {
-        t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, r.LastInsertId)
-    }
+	// Insert
+	q := NewQuery(qt.Query.Server)
+	q.InsertInto("passport_user")
+	q.SetPrimary("UserID") // PostgreSQL compatibility
+	r, err := q.Fields("UserID", "CreationTime", "BirthYear", "Gender", "Nickname").Values(1000000, "2015-01-17 00:00:00", 1980, "Male", "肯·汤普逊").Exec()
+	if err != nil {
+		t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
+	}
+	if r.LastInsertId != 1000000 {
+		t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, r.LastInsertId)
+	}
 
+	// Insert confirm
+	d := make(Item)
+	q = NewQuery(qt.Query.Server)
+	err = q.Select("*").From("passport_user").Where(q.Eq("UserID", 1000000)).Row(&d)
+	if err != nil {
+		t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
+	}
+	qt.dataValidation(t, string(d["CreationTime"].([]byte)), "2015-01-17 00:00:00")
+	qt.dataValidation(t, d["BirthYear"], int64(1980))
+	qt.dataValidation(t, string(d["Gender"].([]byte)), "Male")
+	qt.dataValidation(t, string(d["Nickname"].([]byte)), "肯·汤普逊")
 
-    // Insert confirm
-    d := make(Item)
-    q = NewQuery(qt.Query.Server)
-    err = q.Select("*").From("passport_user").Where(q.Eq("UserID", 1000000)).Row(&d)
-    if err != nil {
-        t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
-    }
-    qt.dataValidation(t, d["CreationTime"], "2015-01-17 00:00:00")
-    qt.dataValidation(t, d["BirthYear"], int64(1980))
-    qt.dataValidation(t, d["Gender"], "Male")
-    qt.dataValidation(t, d["Nickname"], "肯·汤普逊")
+	// Insert
+	d = Item{
+		"UserID":       1000001,
+		"CreationTime": "2015-01-17 01:00:00",
+		"BirthYear":    1986,
+		"Gender":       "Secret",
+		"Nickname":     "阿里马马"}
+	q = NewQuery(qt.Query.Server)
+	q.SetPrimary("UserID") // PostgreSQL compatibility
+	r, err = q.InsertInto("passport_user").Exec(d)
+	if err != nil {
+		t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
+	}
+	if r.LastInsertId != 1000001 {
+		t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, r.LastInsertId)
+	}
 
-
-    // Insert
-    d = Item{
-        "UserID": 1000001,
-        "CreationTime": "2015-01-17 01:00:00",
-        "BirthYear": 1986,
-        "Gender": "Secret",
-        "Nickname": "阿里马马"}
-    q = NewQuery(qt.Query.Server)
-    q.SetPrimary("UserID") // PostgreSQL compatibility
-    r, err = q.InsertInto("passport_user").Exec(d)
-    if err != nil {
-        t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
-    }
-    if r.LastInsertId != 1000001 {
-        t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, r.LastInsertId)
-    }
-
-
-    // Insert confirm
-    d = make(Item)
-    q = NewQuery(qt.Query.Server)
-    err = q.Select("*").From("passport_user").Where(q.Eq("UserID", 1000001)).Row(&d)
-    if err != nil {
-        t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
-    }
-    qt.dataValidation(t, d["CreationTime"], "2015-01-17 01:00:00")
-    qt.dataValidation(t, d["BirthYear"], int64(1986))
-    qt.dataValidation(t, d["Gender"], "Secret")
-    qt.dataValidation(t, d["Nickname"], "阿里马马")
+	// Insert confirm
+	d = make(Item)
+	q = NewQuery(qt.Query.Server)
+	err = q.Select("*").From("passport_user").Where(q.Eq("UserID", 1000001)).Row(&d)
+	if err != nil {
+		t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
+	}
+	qt.dataValidation(t, string(d["CreationTime"].([]byte)), "2015-01-17 01:00:00")
+	qt.dataValidation(t, d["BirthYear"], int64(1986))
+	qt.dataValidation(t, string(d["Gender"].([]byte)), "Secret")
+	qt.dataValidation(t, string(d["Nickname"].([]byte)), "阿里马马")
 }
 
 func (qt *QueryTest) Update(t *testing.T) {
-    // Update
-    q := NewQuery(qt.Query.Server)
-    r, err := q.Update("passport_user").Set("BirthYear", 1982).Set("Gender", "Female").Set("Nickname", "Bob").Where(q.Eq("UserID", 1000000)).Exec()
-    if err != nil {
-        t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
-    }
-    if r.RowsAffected != 1 {
-        t.Fatalf("[%s] Update Failed: %v\n", qt.Query.Server.Type, r.RowsAffected)
-    }
+	// Update
+	q := NewQuery(qt.Query.Server)
+	r, err := q.Update("passport_user").Set("BirthYear", 1982).Set("Gender", "Female").Set("Nickname", "Bob").Where(q.Eq("UserID", 1000000)).Exec()
+	if err != nil {
+		t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
+	}
+	if r.RowsAffected != 1 {
+		t.Fatalf("[%s] Update Failed: %v\n", qt.Query.Server.Type, r.RowsAffected)
+	}
 
+	// Update confirm
+	d := make(Item)
+	w := Where{"UserID": 1000000}
+	q = NewQuery(qt.Query.Server)
+	err = q.Select("*").From("passport_user").Row(&d, w)
+	if err != nil {
+		t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
+	}
+	qt.dataValidation(t, string(d["CreationTime"].([]byte)), "2015-01-17 00:00:00")
+	qt.dataValidation(t, d["BirthYear"], int64(1982))
+	qt.dataValidation(t, string(d["Gender"].([]byte)), "Female")
+	qt.dataValidation(t, string(d["Nickname"].([]byte)), "Bob")
 
-    // Update confirm
-    d := make(Item)
-    w := Where{"UserID": 1000000}
-    q = NewQuery(qt.Query.Server)
-    err = q.Select("*").From("passport_user").Row(&d, w)
-    if err != nil {
-        t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
-    }
-    qt.dataValidation(t, d["CreationTime"], "2015-01-17 00:00:00")
-    qt.dataValidation(t, d["BirthYear"], int64(1982))
-    qt.dataValidation(t, d["Gender"], "Female")
-    qt.dataValidation(t, d["Nickname"], "Bob")
+	// Update
+	d = Item{
+		"BirthYear": 1988,
+		"Gender":    "Male",
+		"Nickname":  "C语言"}
+	w = Where{"UserID": 1000001}
+	q = NewQuery(qt.Query.Server)
+	r, err = q.Update("passport_user").Exec(d, w)
+	if err != nil {
+		t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
+	}
+	if r.RowsAffected != 1 {
+		t.Fatalf("[%s] Update Failed: %v\n", qt.Query.Server.Type, r.RowsAffected)
+	}
 
-
-    // Update
-    d = Item{
-        "BirthYear": 1988,
-        "Gender": "Male",
-        "Nickname": "C语言"}
-    w = Where{"UserID": 1000001}
-    q = NewQuery(qt.Query.Server)
-    r, err = q.Update("passport_user").Exec(d, w)
-    if err != nil {
-        t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
-    }
-    if r.RowsAffected != 1 {
-        t.Fatalf("[%s] Update Failed: %v\n", qt.Query.Server.Type, r.RowsAffected)
-    }
-
-
-    // Update confirm
-    d = make(Item)
-    q = NewQuery(qt.Query.Server)
-    err = q.Select().From("passport_user").Row(&d, w)
-    if err != nil {
-        t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
-    }
-    qt.dataValidation(t, d["CreationTime"], "2015-01-17 01:00:00")
-    qt.dataValidation(t, d["BirthYear"], int64(1988))
-    qt.dataValidation(t, d["Gender"], "Male")
-    qt.dataValidation(t, d["Nickname"], "C语言")
+	// Update confirm
+	d = make(Item)
+	q = NewQuery(qt.Query.Server)
+	err = q.Select().From("passport_user").Row(&d, w)
+	if err != nil {
+		t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
+	}
+	qt.dataValidation(t, string(d["CreationTime"].([]byte)), "2015-01-17 01:00:00")
+	qt.dataValidation(t, d["BirthYear"], int64(1988))
+	qt.dataValidation(t, string(d["Gender"].([]byte)), "Male")
+	qt.dataValidation(t, string(d["Nickname"].([]byte)), "C语言")
 }
 
 func (qt *QueryTest) Delete(t *testing.T) {
-    // Delete
-    q := NewQuery(qt.Query.Server)
-    r, err := q.DeleteFrom("passport_user").Where(q.Eq("UserID", 1000000)).Exec()
-    if err != nil {
-        t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
-    }
-    if r.RowsAffected != 1 {
-        t.Fatalf("[%s] Delete Failed: %v\n", qt.Query.Server.Type, r.RowsAffected)
-    }
+	// Delete
+	q := NewQuery(qt.Query.Server)
+	r, err := q.DeleteFrom("passport_user").Where(q.Eq("UserID", 1000000)).Exec()
+	if err != nil {
+		t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
+	}
+	if r.RowsAffected != 1 {
+		t.Fatalf("[%s] Delete Failed: %v\n", qt.Query.Server.Type, r.RowsAffected)
+	}
 
-    // Delete
-    w := Where{"UserID": 1000001}
-    q = NewQuery(qt.Query.Server)
-    r, err = q.DeleteFrom("passport_user").Exec(w)
-    if err != nil {
-        t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
-    }
-    if r.RowsAffected != 1 {
-        t.Fatalf("[%s] Delete Failed: %v\n", qt.Query.Server.Type, r.RowsAffected)
-    }
+	// Delete
+	w := Where{"UserID": 1000001}
+	q = NewQuery(qt.Query.Server)
+	r, err = q.DeleteFrom("passport_user").Exec(w)
+	if err != nil {
+		t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
+	}
+	if r.RowsAffected != 1 {
+		t.Fatalf("[%s] Delete Failed: %v\n", qt.Query.Server.Type, r.RowsAffected)
+	}
 }
 
 func (qt *QueryTest) Rows(t *testing.T) {
-    // Rows
-    d := []Item{}
-    q := NewQuery(qt.Query.Server)
-    err := q.Select("*").From("passport_user").Where(q.In("UserID", 1000000, 1000001)).Rows(&d)
-    if err != nil {
-        t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
-    }
-    if len(d) != 2 {
-        t.Fatalf("[%s] Returns the number of rows of data is incorrect: %v\n", qt.Query.Server.Type, len(d))
-    }
-    qt.dataValidation(t, d[0]["CreationTime"], "2015-01-17 00:00:00")
-    qt.dataValidation(t, d[0]["BirthYear"], int64(1982))
-    qt.dataValidation(t, d[0]["Gender"], "Female")
-    qt.dataValidation(t, d[0]["Nickname"], "Bob")
+	// Rows
+	d := []Item{}
+	q := NewQuery(qt.Query.Server)
+	err := q.Select("*").From("passport_user").Where(q.In("UserID", 1000000, 1000001)).Rows(&d)
+	if err != nil {
+		t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
+	}
+	if len(d) != 2 {
+		t.Fatalf("[%s] Returns the number of rows of data is incorrect: %v\n", qt.Query.Server.Type, len(d))
+	}
+	qt.dataValidation(t, string(d[0]["CreationTime"].([]byte)), "2015-01-17 00:00:00")
+	qt.dataValidation(t, d[0]["BirthYear"], int64(1982))
+	qt.dataValidation(t, string(d[0]["Gender"].([]byte)), "Female")
+	qt.dataValidation(t, string(d[0]["Nickname"].([]byte)), "Bob")
 
-    // Rows
-    d = []Item{}
-    q = NewQuery(qt.Query.Server)
-    err = q.Select().From("passport_user").Where(q.Eq("UserID", 1000000), q.Or(q.Eq("UserID", 1000001))).Rows(&d)
-    if err != nil {
-        t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
-    }
-    if len(d) != 2 {
-        t.Fatalf("[%s] Returns the number of rows of data is incorrect: %v\n", qt.Query.Server.Type, len(d))
-    }
-    qt.dataValidation(t, d[0]["CreationTime"], "2015-01-17 00:00:00")
-    qt.dataValidation(t, d[0]["BirthYear"], int64(1982))
-    qt.dataValidation(t, d[0]["Gender"], "Female")
-    qt.dataValidation(t, d[0]["Nickname"], "Bob")
+	// Rows
+	d = []Item{}
+	q = NewQuery(qt.Query.Server)
+	err = q.Select().From("passport_user").Where(q.Eq("UserID", 1000000), q.Or(q.Eq("UserID", 1000001))).Rows(&d)
+	if err != nil {
+		t.Fatalf("[%s]: %v\n", qt.Query.Server.Type, err)
+	}
+	if len(d) != 2 {
+		t.Fatalf("[%s] Returns the number of rows of data is incorrect: %v\n", qt.Query.Server.Type, len(d))
+	}
+	qt.dataValidation(t, string(d[0]["CreationTime"].([]byte)), "2015-01-17 00:00:00")
+	qt.dataValidation(t, d[0]["BirthYear"], int64(1982))
+	qt.dataValidation(t, string(d[0]["Gender"].([]byte)), "Female")
+	qt.dataValidation(t, string(d[0]["Nickname"].([]byte)), "Bob")
 }
 
 func (qt *QueryTest) dataValidation(t *testing.T, l, r interface{}) {
-    if l != r {
-        t.Fatalf("[%s] Value validation fails: %v\t%v\n", qt.Query.Server.Type, l, r)
-    }
+	if l != r {
+		t.Fatalf("[%s] Value validation fails: %v\t%v\n", qt.Query.Server.Type, l, r)
+	}
 }
 
 func NewQueryTest(typ string, dsn string) *QueryTest {
-    s := NewServer(typ, typ, dsn)
-    qt := QueryTest{}
-    qt.Query = NewQuery(s)
-    return &qt
+	s := NewServer(typ, typ, dsn)
+	qt := QueryTest{}
+	qt.Query = NewQuery(s)
+	return &qt
 }
 
 func TestQuery(t *testing.T) {
-    st := NewQueryTest("mysql", "root:@tcp(127.0.0.1:3306)/zhgo?charset=utf8")
-    st.Init(t)
-    st.Load(t)
-    st.Start(t)
+	st := NewQueryTest("mysql", "root:@tcp(127.0.0.1:3306)/zhgo?charset=utf8")
+	st.Init(t)
+	st.Load(t)
+	st.Start(t)
 
-    st = NewQueryTest("sqlite3", "sqlite3.db")
-    st.Init(t)
-    st.Load(t)
-    st.Start(t)
+	st = NewQueryTest("sqlite3", "sqlite3.db")
+	st.Init(t)
+	st.Load(t)
+	st.Start(t)
 
-    st = NewQueryTest("postgres", "user=postgres dbname=zhgo sslmode=disable")
-    //st = NewServerTest("postgres", "postgres://LD:@localhost:5432/zhgo?sslmode=verify-full")
-    st.Init(t)
-    st.Load(t)
-    st.Start(t)
+	st = NewQueryTest("postgres", "user=postgres dbname=zhgo sslmode=disable")
+	//st = NewServerTest("postgres", "postgres://LD:@localhost:5432/zhgo?sslmode=verify-full")
+	st.Init(t)
+	st.Load(t)
+	st.Start(t)
 }
--- server_test.go
+++ server_test.go
@@ -5,10 +5,10 @@
 package db
 
 import (
-    "io/ioutil"
-    "testing"
-    "fmt"
-    "strings"
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"testing"
 )
 
 /*
@@ -18,28 +18,28 @@ GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, LOCK TABLES ON `zhgo`
 */
 
 type ServerTest struct {
-    // Server
-    Server *Server
+	// Server
+	Server *Server
 }
 
 func (st *ServerTest) Init(t *testing.T) {
-    // Load sql file
-    p := fmt.Sprintf("tb-%s.sql", st.Server.Type)
-    b, err := ioutil.ReadFile(p)
-    if err != nil {
-        t.Fatalf("[%s]: %v\n", st.Server.Type, err)
-    }
-    initSQL := strings.Split(string(b), ";")
+	// Load sql file
+	p := fmt.Sprintf("tb-%s.sql", st.Server.Type)
+	b, err := ioutil.ReadFile(p)
+	if err != nil {
+		t.Fatalf("[%s]: %v\n", st.Server.Type, err)
+	}
+	initSQL := strings.Split(string(b), ";")
 
-    // Run Init SQL
-    for _, v := range initSQL {
-        if strings.Trim(v, "\r\n \t") != "" {
-            _, err := st.Server.Exec(v)
-            if err != nil {
-                t.Fatalf("[%s]: %v\n", st.Server.Type, err)
-            }
-        }
-    }
+	// Run Init SQL
+	for _, v := range initSQL {
+		if strings.Trim(v, "\r\n \t") != "" {
+			_, err := st.Server.Exec(v)
+			if err != nil {
+				t.Fatalf("[%s]: %v\n", st.Server.Type, err)
+			}
+		}
+	}
 }
 
 func (st *ServerTest) Load(t *testing.T) {
@@ -47,142 +47,140 @@ func (st *ServerTest) Load(t *testing.T) {
 }
 
 func (st *ServerTest) Start(t *testing.T) {
-    st.Insert(t)
-    st.Update(t)
-    st.Rows(t)
-    st.Delete(t)
+	st.Insert(t)
+	st.Update(t)
+	st.Rows(t)
+	st.Delete(t)
 }
 
 func (st *ServerTest) Insert(t *testing.T) {
-    // Insert
-    vs := []interface{}{1000000, "2015-01-17 00:00:00", 1980, "Male", "肯·汤普逊"}
-    q := `INSERT INTO "passport_user" ("UserID", "CreationTime", "BirthYear", "Gender", "Nickname") VALUES($1, $2, $3, $4, $5)`
-    if st.Server.Type == "postgres" {
-        q = `INSERT INTO "passport_user" ("UserID", "CreationTime", "BirthYear", "Gender", "Nickname") VALUES($1, $2, $3, $4, $5) RETURNING *`
-        row := make(Item)
-        err := st.Server.Row(&row, q, vs...)
-        if err != nil {
-            t.Fatalf("[%s]: %v\n", st.Server.Type, err)
-        }
-        lastInsertId, ok := row["UserID"]
-        if !ok {
-            t.Fatalf("[%s]: Insert failed\n", st.Server.Type)
-        }
-        if 1000000 != lastInsertId.(int64) {
-            t.Fatalf("[%s]: %v\n", st.Server.Type, lastInsertId)
-        }
-    } else {
-        r, err := st.Server.Exec(q, vs...)
-        if err != nil {
-            t.Fatalf("[%s]: %v\n", st.Server.Type, err)
-        }
-        lastInsertId, err := r.LastInsertId()
-        if err != nil {
-            t.Fatalf("[%s]: %v\n", st.Server.Type, err)
-        }
-        if lastInsertId != 1000000 {
-            t.Fatalf("[%s]: %v\n", st.Server.Type, lastInsertId)
-        }
-    }
+	// Insert
+	vs := []interface{}{1000000, "2015-01-17 00:00:00", 1980, "Male", "肯·汤普逊"}
+	q := `INSERT INTO "passport_user" ("UserID", "CreationTime", "BirthYear", "Gender", "Nickname") VALUES($1, $2, $3, $4, $5)`
+	if st.Server.Type == "postgres" {
+		q = `INSERT INTO "passport_user" ("UserID", "CreationTime", "BirthYear", "Gender", "Nickname") VALUES($1, $2, $3, $4, $5) RETURNING *`
+		row := make(Item)
+		err := st.Server.Row(&row, q, vs...)
+		if err != nil {
+			t.Fatalf("[%s]: %v\n", st.Server.Type, err)
+		}
+		lastInsertId, ok := row["UserID"]
+		if !ok {
+			t.Fatalf("[%s]: Insert failed\n", st.Server.Type)
+		}
+		if 1000000 != lastInsertId.(int64) {
+			t.Fatalf("[%s]: %v\n", st.Server.Type, lastInsertId)
+		}
+	} else {
+		r, err := st.Server.Exec(q, vs...)
+		if err != nil {
+			t.Fatalf("[%s]: %v\n", st.Server.Type, err)
+		}
+		lastInsertId, err := r.LastInsertId()
+		if err != nil {
+			t.Fatalf("[%s]: %v\n", st.Server.Type, err)
+		}
+		if lastInsertId != 1000000 {
+			t.Fatalf("[%s]: %v\n", st.Server.Type, lastInsertId)
+		}
+	}
 
-
-    // Insert confirm
-    d := make(Item)
-    q = `SELECT * FROM "passport_user" WHERE "UserID" = $1`
-    err := st.Server.Row(&d, q, 1000000)
-    if err != nil {
-        t.Fatalf("[%s]: %v\n", st.Server.Type, err)
-    }
-    st.dataValidation(t, d["CreationTime"], "2015-01-17 00:00:00")
-    st.dataValidation(t, d["BirthYear"], int64(1980))
-    st.dataValidation(t, d["Gender"], "Male")
-    st.dataValidation(t, d["Nickname"], "肯·汤普逊")
+	// Insert confirm
+	d := make(Item)
+	q = `SELECT * FROM "passport_user" WHERE "UserID" = $1`
+	err := st.Server.Row(&d, q, 1000000)
+	if err != nil {
+		t.Fatalf("[%s]: %v\n", st.Server.Type, err)
+	}
+	st.dataValidation(t, string(d["CreationTime"].([]byte)), "2015-01-17 00:00:00")
+	st.dataValidation(t, d["BirthYear"], int64(1980))
+	st.dataValidation(t, string(d["Gender"].([]byte)), "Male")
+	st.dataValidation(t, string(d["Nickname"].([]byte)), "肯·汤普逊")
 }
 
 func (st *ServerTest) Update(t *testing.T) {
-    // Update
-    q := `UPDATE "passport_user" SET "BirthYear" = $1, "Gender" = $2, "Nickname" = $3 WHERE "UserID" = $4`
-    r, err := st.Server.Exec(q, 1982, "Female", "Bob", 1000000)
-    if err != nil {
-        t.Fatalf("[%s]: %v\n", st.Server.Type, err)
-    }
-    rowsAffected, err := r.RowsAffected()
-    if err != nil {
-        t.Fatalf("[%s]: %v\n", st.Server.Type, err)
-    }
-    if rowsAffected <= 0 {
-        t.Fatalf("[%s] Update Failed: %v\n", st.Server.Type, rowsAffected)
-    }
+	// Update
+	q := `UPDATE "passport_user" SET "BirthYear" = $1, "Gender" = $2, "Nickname" = $3 WHERE "UserID" = $4`
+	r, err := st.Server.Exec(q, 1982, "Female", "Bob", 1000000)
+	if err != nil {
+		t.Fatalf("[%s]: %v\n", st.Server.Type, err)
+	}
+	rowsAffected, err := r.RowsAffected()
+	if err != nil {
+		t.Fatalf("[%s]: %v\n", st.Server.Type, err)
+	}
+	if rowsAffected <= 0 {
+		t.Fatalf("[%s] Update Failed: %v\n", st.Server.Type, rowsAffected)
+	}
 
-
-    // Update confirm
-    d := make(Item)
-    q = `SELECT * FROM "passport_user" WHERE "UserID" = $1`
-    err = st.Server.Row(&d, q, 1000000)
-    if err != nil {
-        t.Fatalf("[%s]: %v\n", st.Server.Type, err)
-    }
-    st.dataValidation(t, d["CreationTime"], "2015-01-17 00:00:00")
-    st.dataValidation(t, d["BirthYear"], int64(1982))
-    st.dataValidation(t, d["Gender"], "Female")
-    st.dataValidation(t, d["Nickname"], "Bob")
+	// Update confirm
+	d := make(Item)
+	q = `SELECT * FROM "passport_user" WHERE "UserID" = $1`
+	err = st.Server.Row(&d, q, 1000000)
+	if err != nil {
+		t.Fatalf("[%s]: %v\n", st.Server.Type, err)
+	}
+	st.dataValidation(t, string(d["CreationTime"].([]byte)), "2015-01-17 00:00:00")
+	st.dataValidation(t, d["BirthYear"], int64(1982))
+	st.dataValidation(t, string(d["Gender"].([]byte)), "Female")
+	st.dataValidation(t, string(d["Nickname"].([]byte)), "Bob")
 }
 
 func (st *ServerTest) Delete(t *testing.T) {
-    // Delete
-    q := `DELETE FROM "passport_user" WHERE "UserID" = $1`
-    r, err := st.Server.Exec(q, 1000000)
-    if err != nil {
-        t.Fatalf("[%s]: %v\n", st.Server.Type, err)
-    }
-    rowsAffected, err := r.RowsAffected()
-    if err != nil {
-        t.Fatalf("[%s]: %v\n", st.Server.Type, err)
-    }
-    if rowsAffected != 1 {
-        t.Fatalf("[%s] Delete Failed: %v\n", st.Server.Type, rowsAffected)
-    }
+	// Delete
+	q := `DELETE FROM "passport_user" WHERE "UserID" = $1`
+	r, err := st.Server.Exec(q, 1000000)
+	if err != nil {
+		t.Fatalf("[%s]: %v\n", st.Server.Type, err)
+	}
+	rowsAffected, err := r.RowsAffected()
+	if err != nil {
+		t.Fatalf("[%s]: %v\n", st.Server.Type, err)
+	}
+	if rowsAffected != 1 {
+		t.Fatalf("[%s] Delete Failed: %v\n", st.Server.Type, rowsAffected)
+	}
 }
 
 func (st *ServerTest) Rows(t *testing.T) {
-    d := []Item{}
-    q := `SELECT * FROM "passport_user" WHERE "UserID" > $1`
-    err := st.Server.Rows(&d, q, 1)
-    if err != nil {
-        t.Fatalf("[%s]: %v\n", st.Server.Type, err)
-    }
-    if len(d) != 1 {
-        t.Fatalf("[%s] Returns the number of rows of data is incorrect: %v\n", st.Server.Type, len(d))
-    }
-    st.dataValidation(t, d[0]["CreationTime"], "2015-01-17 00:00:00")
-    st.dataValidation(t, d[0]["BirthYear"], int64(1982))
-    st.dataValidation(t, d[0]["Gender"], "Female")
-    st.dataValidation(t, d[0]["Nickname"], "Bob")
+	d := []Item{}
+	q := `SELECT * FROM "passport_user" WHERE "UserID" > $1`
+	err := st.Server.Rows(&d, q, 1)
+	if err != nil {
+		t.Fatalf("[%s]: %v\n", st.Server.Type, err)
+	}
+	if len(d) != 1 {
+		t.Fatalf("[%s] Returns the number of rows of data is incorrect: %v\n", st.Server.Type, len(d))
+	}
+	st.dataValidation(t, string(d[0]["CreationTime"].([]byte)), "2015-01-17 00:00:00")
+	st.dataValidation(t, d[0]["BirthYear"], int64(1982))
+	st.dataValidation(t, string(d[0]["Gender"].([]byte)), "Female")
+	st.dataValidation(t, string(d[0]["Nickname"].([]byte)), "Bob")
 }
 
 func (st *ServerTest) dataValidation(t *testing.T, l, r interface{}) {
-    if l != r {
-        t.Fatalf("[%s] Value validation fails: %v\t%v\n", st.Server.Type, l, r)
-    }
+	if l != r {
+		t.Fatalf("[%s] Value validation fails: %v\t%v\n", st.Server.Type, l, r)
+	}
 }
 
 func testServer(typ string, dsn string) *ServerTest {
-    s := ServerTest{}
-    s.Server = NewServer(typ, typ, dsn)
-    return &s
+	s := ServerTest{}
+	s.Server = NewServer(typ, typ, dsn)
+	return &s
 }
 
 func TestServer(t *testing.T) {
-    st := testServer("mysql", "root:@tcp(127.0.0.1:3306)/zhgo?charset=utf8")
-    st.Init(t)
-    st.Start(t)
+	st := testServer("mysql", "root:@tcp(127.0.0.1:3306)/zhgo?charset=utf8")
+	st.Init(t)
+	st.Start(t)
 
-    st = testServer("sqlite3", "sqlite3.db")
-    st.Init(t)
-    st.Start(t)
+	st = testServer("sqlite3", "sqlite3.db")
+	st.Init(t)
+	st.Start(t)
 
-    st = testServer("postgres", "user=postgres dbname=zhgo sslmode=disable")
-    //st = testServer("postgres", "postgres://LD:@localhost:5432/zhgo?sslmode=verify-full")
-    st.Init(t)
-    st.Start(t)
+	st = testServer("postgres", "user=postgres dbname=zhgo sslmode=disable")
+	//st = testServer("postgres", "postgres://LD:@localhost:5432/zhgo?sslmode=verify-full")
+	st.Init(t)
+	st.Start(t)
 }
